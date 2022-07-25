9729795d350db65cad5aea42d0aaab9ecfa0b3de
daniel.daza1 <danieldaza030@gmail.com>
2022-07-25 09:40:36 -0500

    feat: idk

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from routers import items
-from utils.telegram.mibot import MiBot
-import telegram_send
 import uvicorn
+
+from utils.telegram.mibot import MiBot
+
 
 app = FastAPI()
 
